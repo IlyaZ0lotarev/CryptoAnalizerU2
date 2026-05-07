38e24a417d07944c2ffeefcba0079aeffc46938d
ILYA ZOLOTAREV <zolotarev2694@mail.ru>
2026-05-08 00:13:31 +0400

    add MainRunner

--- src/main/java/com/javarush/zolotarev/MainRunner.java
+++ src/main/java/com/javarush/zolotarev/MainRunner.java
@@ -1,4 +1,10 @@
 package com.javarush.zolotarev;
 
+import com.javarush.zolotarev.view.ConsoleMenu;
+
 public class MainRunner {
+    public static void main(String[] args) {
+        ConsoleMenu menu = new ConsoleMenu();
+        menu.run();
+    }
 }
